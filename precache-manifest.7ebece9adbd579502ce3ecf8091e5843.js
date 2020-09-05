4e4663e4897ddfab3c4da92acd1e28fef86c4d37
Martin Bagic <martin.bagic@outlook.com>
2020-09-05 21:14:39 +0200

    Updates

---
+++ precache-manifest.7ebece9adbd579502ce3ecf8091e5843.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83f81163d392a7b87dd7a48a7b8da683",
+    "url": "/streha/index.html"
+  },
+  {
+    "revision": "26f139e615b262dd8705",
+    "url": "/streha/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7d544c43d55c3052a749",
+    "url": "/streha/static/js/2.604c7e04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/streha/static/js/2.604c7e04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26f139e615b262dd8705",
+    "url": "/streha/static/js/main.360de4f9.chunk.js"
+  },
+  {
+    "revision": "ca398ef8dd432dfdc0c6",
+    "url": "/streha/static/js/runtime-main.b9866aa8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/streha/static/media/logo.5d5d9eef.svg"
+  }
+]);
